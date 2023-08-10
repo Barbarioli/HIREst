fix to normalization and working decompression

compression_algorithms/core.py
@@ -214,16 +214,16 @@ class CompressionAlgorithm:
 
 		#store the variables
 		self.data = data.copy()
+
 		self.N,self.p = data.shape
 
-		self.normalization = np.vstack([np.max(self.data), np.min(self.data)])
-		self.normalization = np.hstack([self.normalization, np.array([self.N, self.p]).reshape(2,1)])
+		self.normalization = np.array([np.max(self.data), np.min(self.data), self.N, self.p])
 
 		#print(self.normalization.shape)
 		#get all attrs between 0 and 1
 		# for i in range(self.p):
 		# 	self.data[:,i] = (self.data[:,i] - self.normalization[1,i])/(self.normalization[0,i] - self.normalization[1,i])
-		self.data = (self.data - np.max(self.data))/(np.max(self.data)-np.min(self.data))
+		self.data = (self.data - self.normalization[1])/(self.normalization[0]-self.normalization[1])
 		#print(np.count_nonzero(np.isnan(data)))
 		self.NORMALIZATION += '.npy'
 		np.save(self.NORMALIZATION, self.normalization)
@@ -264,11 +264,9 @@ class CompressionAlgorithm:
 		data = original.copy()
 		codec = decompressed.copy()
 		N,p = data.shape
+		data = (data - self.normalization[1])/(self.normalization[0] - self.normalization[1])
+		codec = (codec - self.normalization[1])/(self.normalization[0] - self.normalization[1])
 
-		for i in range(p):
-			data[:,i] = (data[:,i] - self.normalization[1,i])/(self.normalization[0,i] - self.normalization[1,i])
-			codec[:,i] = (codec[:,i] - self.normalization[1,i])/(self.normalization[0,i] - self.normalization[1,i])
-		per_col_errors = np.max(np.abs(data - codec), axis=0)
 		"""
 		if plot == True:
 			plt.title('Original vs reconstructed')
@@ -277,7 +275,7 @@ class CompressionAlgorithm:
 			plt.legend()
 			plt.show()
 		"""
-		return {'Linfty': np.max(per_col_errors), 'L1':np.mean(np.abs(data - codec))}
+		return {'Linfty': np.max(np.abs(data - codec)), 'L1':np.mean(np.abs(data - codec))}
 		
 
 def iarray_bitpacking(codes, order='C'):

compression_algorithms/hirest_flattening.py
@@ -21,7 +21,7 @@ warnings.filterwarnings("ignore")
 #implements a univariate sketch
 class HierarchicalSketch():                                                  
 
-    def __init__(self, quantize_thresh,window_thresh, blocksize, pfn, sfn, start_level, flattening_algo='row', flatten_window=2):
+    def __init__(self, quantize_thresh,window_thresh, blocksize, pfn, sfn, start_level, flattening_algo='row', w=8):
         self.error_thresh = quantize_thresh
         self.coderange = np.ceil(1.0/(quantize_thresh*2))
         self.window_error = window_thresh
@@ -30,17 +30,20 @@ class HierarchicalSketch():
         self.pfn = pfn
         self.sfn = sfn
         self.start_level = start_level
+
+       
+
+        self.w = w 
         if flattening_algo == "row":
             self.flattener = BaseFlatten()
         elif flattening_algo == "window":
-            self.flattener = WindowFlatten(w=flatten_window)
+            self.flattener = WindowFlatten(w=self.w)
         elif flattening_algo == "windowContiguous":
-            self.flattener = WindowContiguousFlatten(w=flatten_window)
+            self.flattener = WindowContiguousFlatten(w=self.w)
     
     def quantize(self, x):
         #x = x.copy()
         x = np.rint(x*self.coderange)
-        x = x.astype(np.int16)
         return x
     
     def dequantize(self, x_quant):
@@ -90,96 +93,19 @@ class HierarchicalSketch():
     #image version
     def encode(self, data):
         cpy = data.copy()
-        #print(cpy.shape)
-        #N = data.shape[0]
-        #self.nblks = int(np.rint(N / self.blocksize))
-        hierarchies = []
-
-        
-        curr = cpy
-        hierarchy = [] 
-        residuals = []
-        residuals_l1 = []
-        residuals_l2 = []
-        residuals_linf = []
-        #w_a = []
-
-        for i in range(self.start_level, self.d + 1):
-            
-
-            w_ = len(curr)
-            #print(w_)
-            v = self.window_error_2d(curr, 8, self.window_error)
-            #print(len(v))
-            #print(v)
-            v_quant = self.quantize(v)
-            #print(len(v_quant))
-            vp = self.dequantize(v_quant)
-
-            curr -= vp
-
-            
-            r_linf = np.max(curr)
-            r_l1 = self.pool_mean(np.abs(curr), w_)
-            r_l2 = self.pool_mean(np.square(curr), w_)
-            hierarchy.append(v_quant)
-            residuals_linf.append(np.max(r_linf))
-            residuals_l1.append(np.mean(r_l1))
-
-            residuals_l2.append(np.sqrt(np.mean(r_l2)))
-            
-            hierarchies.append(list(zip(hierarchy, residuals_linf)))
-
-
-        print('Avg l2 residual: ', list(residuals_l2))
-        print('Avg l1 residual: ', list(residuals_l1))
-        print('linf residual: ', list(residuals_linf))
-        return hierarchies
+        v = self.window_error_2d(cpy, self.w, self.window_error)
+        return self.quantize(v)
 
     def decode(self, sketch, error_thresh=0):
-        
-        #start = timer()
-        W = np.zeros((len(sketch), self.blocksize)) #preallocate
-
-        for i, (h,r) in enumerate(sketch, start = self.start_level):
-            
-            dims = h.shape[0]
-            
-            W[i-self.start_level,:] = self.spline_optim(h, self.blocksize // dims)
-            
-            if r < self.error_thresh:
-                break
-        #print('time:', timer()-start, 'error:', r)
-
-        return self.dequantize(np.sum(W,axis=0))
-
+        return self.dequantize(sketch)
 
     #packs all of the data into a single array
     def pack(self, sketch):
-        vectors = []
-        for h,r in sketch:
-            # vector = np.concatenate([np.array([r]), h.flatten()])
-            vector = np.concatenate([np.array([r]), self.flattener.flatten(h)])
-            vectors.append(vector)
-            #print(vectors)
-        return vectors
+        return self.flattener.flatten(sketch)
 
     #unpack all of the data
     def unpack(self, array, w=2, error_thresh=0):
-        sketch = []
-        for i in range(self.start_level,self.d+1):
-            
-            r = array[0]
-            block = np.zeros()
-            h = array[1:2**i+1]
-            array = array[2**i+1:]
-                
-            sketch.append((h,r))
-            
-            if r < error_thresh:
-                break
-
-        return sketch
+        return self.flattener.unflatten(array)
 
 
 class MultivariateHierarchical(CompressionAlgorithm):
@@ -201,29 +127,15 @@ class MultivariateHierarchical(CompressionAlgorithm):
 
         self.pfn = pfn
 
-        self.sketch = HierarchicalSketch(self.error_thresh, blocksize=blocksize,window_thresh=self.window_error, start_level = self.start_level, pfn=self.pfn, sfn='nearest', flattening_algo=flattening_algo, flatten_window=flatten_window)
-
+        self.sketch = HierarchicalSketch(self.error_thresh, blocksize=blocksize,window_thresh=self.window_error, start_level = self.start_level, pfn=self.pfn, sfn='nearest', flattening_algo=flattening_algo, w=8)
+        cr = self.sketch.coderange
+        self.dtype = np.int8 if cr <= 256 else np.int16 if cr < 65536 else np.int32
+    
     def compress(self):
-
         start = timer()
-        arrays = []
         
         #print(self.data.dtype)
-        
-        ens = self.sketch.encode(self.data)
-        #print(len(ens[0]))
-        
-
-            
-        for en in ens:
-                #cumulative_gap = min(self.error_thresh - en[-1][1], cumulative_gap)
-            arrays.append(self.sketch.pack(en))
-
-        codes = np.vstack(arrays).astype(np.float16)
-
-        
-        #fname = self.CODES
-
+        codes = self.sketch.pack(self.sketch.encode(self.data)).astype(self.dtype)
         trc_flag = '-' + self.TURBO_CODE_PARAMETER
         # flush to .npy file
         self.path = self.CODES + '.npy'
@@ -231,6 +143,8 @@ class MultivariateHierarchical(CompressionAlgorithm):
         np.save(self.path, codes)
         self.CODES += '.rc'
         self.DATA_FILES[0] = self.CODES
+
+
         print('\n')
         
         # run TRC [compression]
@@ -250,63 +164,19 @@ class MultivariateHierarchical(CompressionAlgorithm):
         #subprocess.run(['./../Turbo-Range-Coder/turborc', '-d', self.CODES, self.path])
         command = " ".join([self.TURBO_CODE_LOCATION, "-d", self.CODES, self.path])
         os.system(command)
-        
-        packed = np.load(self.path)
-        print('\n')
-
-        #unpack_time = timer() - start
-        #print('trc time: ', unpack_time)
-        packed = packed.reshape(self.p*self.sketch.nblks, -1, order='F')
-        
-        #start = timer()
-
 
         normalization = np.load(self.NORMALIZATION)
-        _, P2 = normalization.shape
+        #self.normalization = np.array([np.max(self.data), np.min(self.data), self.N, self.p])
 
-        p = int(P2 - 1)
-        N = int(normalization[0,p])
-        codes = np.zeros((N,p))
+        # N, p = self.normalization[2], self.normalization[3]
+        
+        codes = self.sketch.unpack(self.sketch.decode(np.load(self.path)))
 
-        #normalize_time = timer() - start
-        #print('normalize time: ', normalize_time)
-        #start = timer()
+        codes = codes*(normalization[0] - normalization[1]) + normalization[1]
 
-        j = -1
-        #k=0
-        #index the vstacked arrs
-        start = timer()
-        for i in range(self.p*self.sketch.nblks):
-            # detects new column
-            if i % self.sketch.nblks == 0:
-                # index og codes
-                k = 0
-                # index blocks
-                j += 1
-            #start = timer()
-            sk = self.sketch.unpack(packed[i,:], error_thresh)
-            #unpack_time = timer() - start
-            #print('unpack time: ', unpack_time)
-
-            # print('decompress end', sk)
-            #start = timer()
-            codes[k*self.blocksize:(k+1)*self.blocksize, j] = self.sketch.decode(sk, error_thresh)
-            #decode_time = timer() - start
-            #print('decode time: ', decode_time)
-            k += 1
-
-
-        for i in range(p):
-            codes[:,i] = (codes[:,i])*(normalization[0,i] - normalization[1,i]) + normalization[1,i]
-
-
-        #denormalize_time = timer() - start
-        #print('denormalize time: ', denormalize_time)
 
         self.compression_stats['decompression_latency'] = timer() - start
-        #self.compression_stats['decompression_ratio'] = (codes.size * codes.itemsize)/self.compression_stats['original_size']
         if not original is None:
-            #print(original-codes)
             self.compression_stats['errors'] = self.verify(original, codes)
 
         return codes
@@ -318,29 +188,43 @@ Test code here
 ####
 
 """"""
+
+# packing algo experiments
+# if __name__ == "__main__":
+#     # for w in [2,4,8,16,32]:
+#     for w in [8]:
+#         ratios, latencies, = [], []
+#         for data in os.listdir('data'):
+#             if data.endswith('.f32'):
+#                 file = np.fromfile('data/' + data, dtype=float)
+#                 file = file.reshape(1800, 1800)
+#                 data = file[0:1024,0:1024]
+#                 shape = 1024
+#                 ratios_1ds = []
+#                 latencies_1ds = []
+#                 for flattener_type in ['row', 'window', 'windowContiguous']:
+#                     nn = MultivariateHierarchical('hier', quantize_thresh = 0.005,window_thresh = 0.005, blocksize=shape, start_level = 10, trc = True, flattening_algo=flattener_type, flatten_window=w)
+#                     nn.load(data)
+#                     nn.compress()
+#                     ratios_1ds.append(nn.compression_stats['compressed_ratio'])
+#                     latencies_1ds.append(nn.compression_stats['compression_latency'])
+#                 ratios.append(ratios_1ds)
+#                 latencies.append(latencies_1ds)
+
+#         ratios = np.array(ratios)
+#         latencies = np.array(latencies)
+#         np.savetxt("packing_algo_results/ratios_{}.csv".format(w), ratios, delimiter=',', fmt='%f')
+#         np.savetxt("packing_algo_results/latencies_{}.csv".format(w), latencies, delimiter=',', fmt='%f')   
+
 if __name__ == "__main__":
-    # for w in [2,4,8,16,32]:
-    for w in [8]:
-        ratios, latencies, = [], []
-        for data in os.listdir('data'):
-            if data.endswith('.f32'):
-                file = np.fromfile('data/' + data, dtype=float)
-                file = file.reshape(1800, 1800)
-                data = file[0:1024,0:1024]
-                shape = 1024
-                ratios_1ds = []
-                latencies_1ds = []
-                for flattener_type in ['row', 'window', 'windowContiguous']:
-                    nn = MultivariateHierarchical('hier', quantize_thresh = 0.005,window_thresh = 0.005, blocksize=shape, start_level = 10, trc = True, flattening_algo=flattener_type, flatten_window=w)
-                    nn.load(data)
-                    nn.compress()
-                    ratios_1ds.append(nn.compression_stats['compressed_ratio'])
-                    latencies_1ds.append(nn.compression_stats['compression_latency'])
-                ratios.append(ratios_1ds)
-                latencies.append(latencies_1ds)
+    file = np.fromfile('data/CLDLOW_1_1800_3600.f32', dtype=float)
+    file = file.reshape(1800, 1800)
+    data = file[0:1024,0:1024]
 
-        ratios = np.array(ratios)
-        latencies = np.array(latencies)
-        np.savetxt("packing_algo_results/ratios_{}.csv".format(w), ratios, delimiter=',', fmt='%f')
-        np.savetxt("packing_algo_results/latencies_{}.csv".format(w), latencies, delimiter=',', fmt='%f')   
+    shape = 1024
+    nn = MultivariateHierarchical('hier', quantize_thresh = 0.005, window_thresh = 0.005, blocksize=shape, start_level = 10, trc = True)
 
+    nn.load(data)
+    nn.compress()
+    nn.decompress(data) 
+    print(nn.compression_stats)