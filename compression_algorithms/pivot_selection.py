335d58ba75667fe73849e54f2b463f74f8e5acf6
Gabe Mersy <gabemersy@gmail.com>
2023-08-29 17:53:32 -0500

    a few replacement updates

--- compression_algorithms/pivot_selection.py
+++ compression_algorithms/pivot_selection.py
@@ -5,39 +5,63 @@ import matplotlib.pyplot as plt
 from abc import ABC, abstractmethod
 
 class WindowReplacement:
-    def window_error_2d(self, x, width, error, first_element = True, pool_function = False):
+    def __init__(self, first_element = True):
+        self.first_element = first_element
+    def window_error_2d(self, x, width, error):
         w,l = x.shape[0], x.shape[1]
-        for i in range(0, w-width+1, int(width)):
-            for j in range(0, l-width+1, int(width)):
-                window = x[i:i+width,j:j+width]
-                if first_element:
-                    pivot = x[i,j]
-                else:
-                    pivot  = np.mean(window)
-                window[np.abs(window - pivot) < error] = pivot
-                x[i:i+width,j:j+width] = window
+        if self.first_element:
+            for i in range(0, w-width+1, width):
+                for j in range(0, l-width+1, width):
+                    window = x[i:i+width,j:j+width]
+                    pivot = window[0,0]
+                    window[np.abs(window - pivot) < error] = pivot
+                    x[i:i+width,j:j+width] = window
+        else:
+            for i in range(0, w-width+1, width):
+                for j in range(0, l-width+1, width):
+                    window = x[i:i+width,j:j+width]
+                    pivot  = window[np.random.randint(0,width), np.random.randint(0,width)]
+                    window[np.abs(window - pivot) < error] = pivot
+                    x[i:i+width,j:j+width] = window
         return x
 
 class WindowReplacementHistogram:
-    def __init__(self, samp_percent, bins):
+    def __init__(self, samp_percent, bins, midpoint=True):
         self.samp_percent = samp_percent
         self.bins = bins
+        self.midpoint = midpoint
 
     def window_error_2d(self, x, width, error):
         samp_percent = self.samp_percent
         bins = self.bins
-        w,l = x.shape[0], x.shape[1]
         n = int(np.floor(width*width*samp_percent))
-        for i in range(0, w-width+1, int(width)):
-            for j in range(0, l-width+1, int(width)):
-                window = x[i:i+width,j:j+width]
-                smp = np.random.choice(window.flatten(), n)
-                hist, buckets = np.histogram(smp, bins = bins)
-                max_bucket = np.argmax(hist)
-                # midpoint of the bucket
-                pivot = (buckets[max_bucket] + buckets[max_bucket+1])/2
-                window[np.abs(window - pivot) < error] = pivot
-                x[i:i+width,j:j+width] = window
+        w,l = x.shape[0], x.shape[1]
+
+        if self.midpoint:
+            for i in range(0, w-width+1, width):
+                for j in range(0, l-width+1, width):
+                    window = x[i:i+width,j:j+width]
+                    smp = np.random.choice(window.flatten(), n)
+                    hist, buckets = np.histogram(smp, bins = bins)
+                    max_bucket = np.argmax(hist)
+                    pivot = (buckets[max_bucket] + buckets[max_bucket+1])/2
+                    window[np.abs(window - pivot) < error] = pivot
+                    x[i:i+width,j:j+width] = window
+        else:
+            for i in range(0, w-width+1, width):
+                for j in range(0, l-width+1, width):
+                    window = x[i:i+width,j:j+width]
+                    smp = np.random.choice(window.flatten(), n)
+                    hist, buckets = np.histogram(smp, bins = bins)
+                    max_bucket = np.argmax(hist)
+                    bucket_min = buckets[max_bucket]
+                    bucket_max = buckets[max_bucket+1]
+                    for s in range(n):
+                        if smp[s] >= bucket_min and smp[s] < bucket_max:
+                                pivot = smp[s]
+                                break
+                    window[np.abs(window - pivot) < error] = pivot
+                    x[i:i+width,j:j+width] = window
         return x
     
 class WindowReplacementKDE:
@@ -45,11 +69,11 @@ class WindowReplacementKDE:
         self.samp_percent = samp_percent
     
     def window_error_2d(self, x, width, error):
-        w,l = x.shape[0], x.shape[1]
         samp_percent = self.samp_percent
         n = int(np.floor(width*width*samp_percent))
-        for i in range(0, w-width+1, int(width)):
-            for j in range(0, l-width+1, int(width)):
+        w, l = x.shape[0], x.shape[1]
+        for i in range(0, w-width+1, width):
+            for j in range(0, l-width+1, width):
                 window = x[i:i+width,j:j+width]
                 smp = np.random.choice(window.flatten(), n)
                 pivot = smp[np.argmax(gaussian_kde(smp).pdf(smp))]
@@ -63,9 +87,9 @@ class WindowReplacementAdaptivePivot:
     
     def window_error_2d(self, x, width, error):
         new_pivot_number = self.new_pivot_number
-        w,l = x.shape[0], x.shape[1]
-        for i in range(0, w-width+1, int(width)):
-            for j in range(0, l-width+1, int(width)):
+        w, l = x.shape[0], x.shape[1]
+        for i in range(0, w-width+1, width):
+            for j in range(0, l-width+1, width):
                 counter = 0
                 for k in range(i,i+width):
                     for y in range(j,j+width):
@@ -85,43 +109,37 @@ if __name__ == '__main__':
     x = file[0:1024,0:1024]
 
     x = (x - np.min(x))/(np.max(x) - np.min(x))
+    print(np.max(x), np.min(x))
     og = x.copy()
     # x =np.random.normal(0,1,(1000,1000))
-    wr = WindowReplacement()
-    window = 8
+    wr = WindowReplacement(first_element=True)
+    window = 16
     error = 0.001
     print("Window size: ", window)
+    
 
     start = time()
-    new_x = wr.window_error_2d(x, window, error, first_element = True)
+    new_x = wr.window_error_2d(x, window, error)
     end = time()
+    
 
     # get max error
     print(np.max(np.abs(og - new_x)))
     print('First element time: ', round(end-start, 2))
 
-    # wr = WindowReplacement()
-    # start = time()
-    # new_x = wr.window_error_2d_hist(x, window, error, sampling_prcnt, 10)
-    # end = time()
-    # print('Hist: ', wr.replacements)
-    # print('Hist time: ', round(end-start, 2))
+    x = og
+    og = og.copy()
+    x = (x - np.min(x))/(np.max(x) - np.min(x))
+    print(np.max(x), np.min(x))
 
-    # # adaptive pivot
-    # wr = WindowReplacement()
-    # start = time()
-    # new_x = wr.window_error_2d_adaptive_pivot(x, window, 16, error)
-    # end = time()
-    # print('Adaptive pivot: ', wr.replacements)
-    # print('Adaptive pivot time: ', round(end-start, 2))
+    # histogram
+    wr = WindowReplacementHistogram(0.1, 10)
+    start = time()
+    new_x = wr.window_error_2d(x, window, error)
+    end = time()
+    print(np.max(np.abs(og - new_x)))
+    print('Histogram time: ', round(end-start, 2))
 
-    # # kde
-    # wr = WindowReplacement()
-    # start = time()
-    # new_x = wr.window_error_2d_kde(x, window, error, sampling_prcnt)
-    # end = time()
-    # print('KDE: ', wr.replacements)
-    # print('KDE time: ', round(end-start, 2))
 
 
 
